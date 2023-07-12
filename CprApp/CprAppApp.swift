7ab4e95b5affc55eec051fc3ac763a944c18278f
Ricky Aguilar <r3aguila@ucsd.edu>
2023-07-12 01:20:12 -0700

    Initial Commit

---
+++ CprApp/CprAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CprAppApp.swift
+//  CprApp
+//
+//  Created by Ricardo Aguilar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CprAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
